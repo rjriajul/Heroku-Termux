8b7cb530044d9b7436e04ee454376ede49b36e60
Riajul <93116400+rjriajul@users.noreply.github.com>
2025-05-12 14:38:54 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,35 @@
-pkg install wget tar gzip nodejs -y
+# Install dependencies
+pkg update -y
+pkg install wget tar gzip nodejs bash -y
 
-# Download and extract Heroku CLI
+# Clean any previous broken Heroku install
+rm -rf $PREFIX/lib/heroku
+rm -f $PREFIX/bin/heroku
+
+# Download Heroku CLI (ARM Linux build)
 wget https://cli-assets.heroku.com/channels/stable/heroku-linux-arm.tar.gz -O heroku.tar.gz
 tar -xvzf heroku.tar.gz
 
-# Move files to Termux lib directory
+# Move Heroku files
 mkdir -p $PREFIX/lib/heroku
 mv heroku/* $PREFIX/lib/heroku
 rm -rf heroku heroku.tar.gz
 
-# Create symlink to make `heroku` accessible
+# Link CLI binary to $PREFIX/bin
 ln -sf $PREFIX/lib/heroku/bin/heroku $PREFIX/bin/heroku
 
-# Fix Node path for Termux
+# Fix node binary path
 cd $PREFIX/lib/heroku/bin
 rm -f node
 ln -s $PREFIX/bin/node node
 
-# Optional: Add Heroku CLI to PATH permanently
-echo 'export PATH="$PREFIX/lib/heroku/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+# Add Heroku to PATH in .bashrc
+if ! grep -q 'lib/heroku/bin' ~/.bashrc; then
+  echo 'export PATH="$PREFIX/lib/heroku/bin:$PATH"' >> ~/.bashrc
+fi
+
+# Start Bash and load new PATH
+exec bash
 
 # Run Heroku CLI
 heroku --version
